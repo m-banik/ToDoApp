59253bfb5d07b99f0426e2b87ab8ff80ba0d9018
Marcin Banik <mrn.banik@gmail.com>
2019-05-14 17:02:26 +0200

    Updates

---
+++ precache-manifest.713c41ffbd83b6079fe101c1bc5d2c6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ba73d416d2e152ec589",
+    "url": "/ToDoApp/static/css/main.4690a64d.chunk.css"
+  },
+  {
+    "revision": "3ba73d416d2e152ec589",
+    "url": "/ToDoApp/static/js/main.3ba73d41.chunk.js"
+  },
+  {
+    "revision": "a6e17a26138b230beb48",
+    "url": "/ToDoApp/static/js/1.a6e17a26.chunk.js"
+  },
+  {
+    "revision": "6ef6ea77c30c55a5d0cf",
+    "url": "/ToDoApp/static/js/runtime~main.6ef6ea77.js"
+  },
+  {
+    "revision": "5c37a32ecaf056cec35e408d5a0142ca",
+    "url": "/ToDoApp/index.html"
+  }
+];
